2d998dcc4e2a8643b6160ed20cf8c315e4cbb2c0
Brian Wilson <brian@wildsong.biz>
2017-03-01 11:48:39 -0800

    remote access

--- importer.py
+++ importer.py
@@ -6,22 +6,23 @@ from __future__ import print_function
 import sys, os
 from osgeo import ogr
 
-# If set to true just set up commands, don't do database writes
-dryrun = True
-
 shp_driver = ogr.GetDriverByName('ESRI Shapefile')
 fgdb_driver = ogr.GetDriverByName('OpenFileGDB') # There is an ESRI driver too but this one is bundled with GDAL.
 pg_driver = ogr.GetDriverByName('PostgreSQL')
 
-debug = True
+debug = False
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
         
 def dprint(*args, **kwargs):
     if debug:
-        dprint(*args, file=sys.stderr, **kwargs)
+        print(*args, file=sys.stderr, **kwargs)
         
+def sanitize_tablename(tablename):
+    cleaned = tablename.lower().strip().replace(' ','_')
+    return cleaned
+    
 class importer(object):
 
     #  There are two methods for import included here,
@@ -32,23 +33,38 @@ class importer(object):
     #  TODO: evaluate the merits and say why one is better than the other.
     #
 
+    # If set to true just set up commands, don't do database writes
+    dryrun = False
+
     sref = 3857 # Web Mercator is a default, you should pick something
 
     schema = None              # you have to set this
-
-    username = 'gis_owner'     # These should come from environment
-    password = 'easypassword'
-    dbname = 'gisdata'
-
+    dbname = 'gis_data'
+    hostname = 'localhost'
+    username = 'gis_owner'     # NB password should be in your .pgpass file if needed.
     dataSink = None
 
     def __init__(self, schema=None, sref=3857):
+        self.schema = schema
+        self.sref = sref
         pass
 
-    def _ogr_import(shapefilename, db):
+    def create_schema(self):
+        # TODO: only create if it does not already exist
+
+        password = self.password
+        if password == None:
+            password_arg = '--no-password' # Try running w/o a password.
+
+        schemacmd = 'psql -c "CREATE SCHEMA %s;" -h %s -U %s --no-password --dbname=%s' % (self.schema, self.hostname, self.username, self.dbname)
+        if not self.dryrun:
+            os.system(schemacmd)
+        return
+
+    def _ogr_import(shapefilename, tablename):
         """ Import a shapefile into a database table. """
 
-        connstr  = "PG:dbname='%s' user='%s' password='%s' active_schema=%s" % (self.dbname, self.username, self.password, self.schema)
+        connstr  = "PG:dbname='%s' active_schema=%s username='%s'" % (self.dbname, self.schema, self.username)
         if not self.dataSink:
             try:
                 dataSink = pg_driver.Open(connstr, True)
@@ -69,51 +85,71 @@ class importer(object):
         layer = dataSource.GetLayer(0)
         layerName = layer.GetName();
     
-        dprint("Copying %d features from %s to %s" % (layer.GetFeatureCount(), layerName, connstr + ' ' + db))
-        if not dryrun:
-            pg_layer = dataSink.CopyLayer(layer, db)
+        dprint("Copying %d features from %s to %s" % (layer.GetFeatureCount(), layerName, connstr + ' ' + tablename))
+        if not self.dryrun:
+            pg_layer = dataSink.CopyLayer(layer, tablename)
             if pg_layer is None:
-                eprint("Layer copy failed on", db)
+                eprint("Layer copy failed on", tablename)
 
         return
 
-    def _shp2pgsql_import(self, shapefilename, db):
+    def _shp2pgsql_import(self, shapefilename, tablename):
     
         # d = drop table (generates a non-fatal error if it does not exist)
         # s = spatial reference (could be -s from:to)
-    
-        shpcmd  = "shp2pgsql -d -s %d %s %s.%s" % (sref, shapefilename, schema, db)
-        psqlcmd = "psql --quiet --username=%s --no-password --dbname=%s" % (username, dbname)
+
+        shpcmd  = "shp2pgsql -d -s %d %s %s.%s" % (self.sref, shapefilename, self.schema, tablename)
+        psqlcmd = "psql --quiet -h %s -U %s --no-password --dbname=%s" % (self.hostname, self.username, self.dbname)
         cmd = shpcmd + " | " + psqlcmd
         dprint(cmd)
-        os.system(cmd)
+        if not self.dryrun:
+            os.system(cmd)
         return
 
     def do_import(self, list):
+        """ NB: This will overwrite out any existing data!!! """
 
-        for (shapefilename, db) in list:
-            if not len(db): db = shapefilename[:-4].lower()
-            pathname = shapefilename # os.path.join(dir, shapefilename)
-            if os.path.exists(pathname):
-                dprint("Importing %s => %s" % (pathname, db))
-                #_ogr_import(shapefilename, db)
-                _shp2pgsql_import(shapefilename, db)
+        if not self.dbname: raise Exception("database name is not set.")
+        if not self.schema: raise Exception("schema is not set.")
+
+        self.create_schema()
+    
+        for (shapefilename, name) in list:
+            tablename = sanitize_tablename(name)
+            if os.path.exists(shapefilename):
+                dprint("Importing %s => %s" % (shapefilename, tablename))
+                #_ogr_import(shapefilename, tablename)
+                self._shp2pgsql_import(shapefilename, tablename)
             else:
                 eprint("Can't find %s" % pathname)
-    return
+        return
 
 ####################################################
 
+def test_sanitizer():
+    l_test = [ "MixedCase", "Spaces in name" ]
+    for i in l_test:
+        print("'%s' => '%s'" % (i, sanitize_tablename(i)))
+
 if __name__ == '__main__':
+
+    debug  = True
+
+    # You have to figure out the correct EPSG code to use for your data.
+    # I suggest feeding a PRJ file to http://prj2epsg.org/search
+
+    #d_county = '/home/GISData/CA/Marin' # directory containing the source data
+    #sref = 2872 # NAD_1983_HARN_StatePlane_California_III_FIPS_0403_Feet
+    #schema   = "ca_co_marin"
+    #username = 'gis_owner'
+    #password = None
+
+    d_county = '/Users/bwilson/ownCloud/DataRepository/OR/LincolnCounty'
+    sref = 2913
+    schema = 'or_co_lincoln'
+    username = os.environ['GISOWNER']
+    hostname = 'dart.wildsong.biz'
     
-    sref = 102243 # NAD_1983_HARN_StatePlane_California_III_FIPS_0403_Feet
-
-    d_county = '/home/GISData/CA/Marin' # directory containing the source data
-    dbname   = 'gisdata'
-    schema   = "ca_co_marin"
-    username = "gis_owner"
-    password = "easypassword"
-
     os.chdir(d_county)
 
 # list.txt contains a list of shapefiles to import
@@ -129,14 +165,21 @@ if __name__ == '__main__':
             pair = item.split()
             if os.path.exists(pair[0]):
                 if len(pair)<2:
-                    pair.append(pair[0])
+                    name,ext = os.path.splitext(pair[0])
+                    pair.append(name)
                 list.append(pair)
             else:
-                eprint("There is no file '%s', skipping it." % pair[0]
+                eprint("There is no file '%s', skipping it." % pair[0])
 
     dprint(list)
 
     imp = importer(schema=schema, sref=sref)
+    #imp.dryrun = True
+    
+    # Using default for username and don't need a password
+    self.username = username
+    self.hostname = hostname
+    
     imp.do_import(list);
 
     sys.exit(0)
